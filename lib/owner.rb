ae64aecf4ec70a15280398c3813345578f2914a7
gillsinaglass <rdgill95@gmail.com>
2019-01-10 14:07:14 -0500

    Teamwork makes the dream work

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,81 @@
+require_relative 'dog.rb'
+require_relative 'cat.rb'
+require_relative 'fish.rb'
+require 'pry'
+
 class Owner
-  # code goes here
-end
+
+  attr_accessor :pets, :name
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @pets = {:cats=>[], :dogs=>[], :fishes=>[]}
+    @@all << self
+    @species = "human"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def say_species
+    return "I am a #{species}."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].map do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].map do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].map do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.map do |pet, array|
+      array.map do |animal|
+      animal.mood = "nervous"
+      @pets[pet] = []
+    end
+   end
+   @pets
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+    end
+
+
+  end
