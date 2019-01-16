7bd4fd63338ea330e3d41a074ae74b3faf138f31
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-01-16 12:50:53 -0500

    Revert "done"
    This reverts commit 267af40838b2c211ca98718da67cea1041f8b1c9.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,34 +1,3 @@
 class Dog
   # code goes here
-
-    attr_reader :name
-    attr_accessor :mood, :owner
-
-    @@all = []
-
-    def initialize(name, owner)
-        @name = name
-        @owner = owner
-        @species = "canine"
-        @mood = "nervous"
-        @@all << self
-    end
-
-    def change_species(new_species)
-        @species = new_species
-    end
-
-    def owned_by
-        puts "I am a dog.  My owner is #{self.owner.name}"
-    end
-
-    def self.all
-        @@all 
-    end
-
-    def self.kill_all_dogs
-        @@all = []
-    end
 end
-
-
